8b1bd51bbdae38851b43ae4ba514a7134ea0c3b8
sunny-mc <168322204+sunny-mc@users.noreply.github.com>
2024-07-10 23:40:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    var nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+// Add class to header and footer on scroll
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('topbar');
+    const footer = document.getElementById('footer');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+        footer.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+        footer.classList.remove('scrolled');
+    }
 });
